5cee8fd4360bbaec623523dc3eeb07ac0d8f50a0
root <giovanniciampi95@gmail.com>
2020-12-13 22:45:58 +0000

    added better visualization

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -5,59 +5,69 @@ import numpy as np
 class LogisticRegression:
     def __init__(self, dim):
         self.dim = dim
-        self.params = np.random.uniform(0.0, 1.0, dim+1)
+        self.params = np.array([0.01 for i in range(dim+1)])
         
     
-    def fit(self, X, y, learning_rate, epochs):
+    def fit(self, X, y, learning_rate, epochs, add_bias=True):
+        y = np.array(y)
+        if add_bias:
+            X = self.__add_bias(X)
+            
         for e in range(epochs):
-            for i in range(self.dim+1):
-                d_i = self.__d_param_i__(X, y, i)
-                self.params[i] = self.params[i] - learning_rate * d_i
+            gradients = self.__get_gradient(X, y)
+            self.params = self.params - learning_rate*gradients
             
         return self.get_cost(X, y)
+
     
+    def __get_gradient(self, X, y):
+        n = X.shape[0]
+        all_preds = self.predict(X, add_bias = False)
+        return (1 / n) * np.dot(X.T,  all_preds - y)
+
         
-    def predict(self, X):
-        predictions = [] 
-        for x in X:
-            predictions.append(self.__predict_point__(x))
-            
-        return predictions
+    def predict(self, X, add_bias=True, thresholded=False, t=.5):
+        if add_bias:
+            X = self.__add_bias(X)
+        preds = np.array([self.__predict_point(x) for x in X])
+        if thresholded:
+            return [1 if p>=t else 0 for p in preds]
+        return preds
          
         
-    def __predict_point__(self, x):
-        prediction = self.params[0]
-        for i in range(self.dim):
-            #print(self.params, x, i)
-            prediction += self.params[i+1] * x[i]  
-            
-        return self.__sigmoid__(prediction)
-        
-        
-    def __d_param_i__(self, X, y, i):
-        s=0
-        
-        for x, t in zip(X, y):
-            prediction=self.__predict_point__(x)
-            temp = prediction - t
-            if i != 0:
-                temp *= x[i-1]
-            s += temp
-        return s
+    def __predict_point(self, x):
+        net = np.dot(x, self.params)
+        return self.__sigmoid(net)
     
     
-    def __sigmoid__(self, x):
+    def __sigmoid(self, x):
         return 1. / (1. + np.exp(-x))
+    
+    def __add_bias(self, X):
+        return np.insert(X.reshape(-1, 1), 0, np.ones(X.shape[0]), axis=1)
             
         
-    def get_cost(self, X, y):
-        n = len(X)
+    def get_cost(self, X, y, add_bias=False):
+        n = X.shape[0]
         s = 0
+        if add_bias:
+            X=self.__add_bias(X)
         
         for x, t in zip(X, y):
-            prediction = self.__predict_point__(x)
+            prediction = self.__predict_point(x)
+            prediction = self.__get_math_safe_prediction(prediction)
             s += t*(log(prediction)) + (1-t)*(log(1-prediction))
         return -1/n*s
     
+    
     def get_params(self):
         return self.params
+    
+    
+    def __get_math_safe_prediction(self, p, adj=0.00001):
+        if p == 0: 
+            return p+adj
+        elif p ==1: 
+            return p-adj
+        else: 
+            return p
